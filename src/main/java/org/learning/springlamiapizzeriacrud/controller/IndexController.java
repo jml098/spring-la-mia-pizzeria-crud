de69987465ca6e9ff213fcf6b1ba244809ab3983
jml098 <jmlrissac@gmail.com>
2023-09-18 13:39:43 +0200

    Added pizza details page

--- src/main/java/org/learning/springlamiapizzeriacrud/controller/IndexController.java
+++ src/main/java/org/learning/springlamiapizzeriacrud/controller/IndexController.java
@@ -3,12 +3,16 @@ package org.learning.springlamiapizzeriacrud.controller;
 import org.learning.springlamiapizzeriacrud.model.Pizza;
 import org.learning.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -25,5 +29,19 @@ public class IndexController {
         List<Pizza> pizzaList = pizzaRepository.findAll();
         model.addAttribute("pizzas", pizzaList);
         return "index";
+    }
+
+    @GetMapping("/{pizzaId}")
+    public String pizza(@PathVariable("pizzaId") Long id, Model model) {
+
+        Optional<Pizza> pizzaOptional = pizzaRepository.findById(id);
+        if (pizzaOptional.isPresent()) {
+            model.addAttribute("pizza", pizzaOptional.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+
+        return "pizza";
     }
 }
